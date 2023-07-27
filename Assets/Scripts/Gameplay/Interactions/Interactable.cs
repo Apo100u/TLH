e2d189538f29084c3256b141f39f59a1ff3bb775
Apo100u <mailodspamu@o2.pl>
2023-07-27 18:35:19 +0200

    develop: create base architecture for interaction system

---
+++ Assets/Scripts/Gameplay/Interactions/Interactable.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace TLH.Gameplay.Interactions
+{
+    public class Interactable : MonoBehaviour
+    {
+        private IInteractionReceiver[] interactionReceivers;
+
+        private void Awake()
+        {
+            interactionReceivers = GetComponents<IInteractionReceiver>();
+        }
+
+        public void HandleInteraction<T>(T interaction) where T : Interaction
+        {
+            for (int i = 0; i < interactionReceivers.Length; i++)
+            {
+                if (interactionReceivers[i] is IInteractionReceiver<T> receiverOfHandledInteraction)
+                {
+                    receiverOfHandledInteraction.HandleInteraction(interaction);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Interactions/Interaction.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace TLH.Gameplay.Interactions
+{
+    public abstract class Interaction : ScriptableObject
+    {
+    }
+}
